b62316d10ca433ec261446a367ecf073e7c6c88e
ksocha <kamil.socha@interia.eu>
2022-01-13 10:39:34 +0100

    opt out rust compiler

--- inversify.config.ts
+++ inversify.config.ts
@@ -1,5 +1,13 @@
 import { Container } from 'inversify';
 
+import TodoMemoryService from 'services/data/TodoMemoryService';
+import ConsoleLogger from 'services/loggers/ConsoleLogger';
+import { Logger } from 'types/Logger';
+import { ToDoService } from 'types/ToDoService';
+
 const container = new Container();
+
+container.bind(Logger.$).to(ConsoleLogger);
+container.bind(ToDoService.$).to(TodoMemoryService);
 
 export default container;
